f9666e43560b970264ec14b26d8fc9f3a88fe16d
Mateusz Grzeskowiak <mateusz.r.grzeskowiak@gmail.com>
2020-02-08 14:36:05 +0100

    First hit added

--- src/main/java/pl/sda/bigOne/App.java
+++ src/main/java/pl/sda/bigOne/App.java
@@ -1,15 +1,22 @@
 package pl.sda.bigOne;
 
 
+import pl.sda.bigOne.actor.Monster;
 import pl.sda.bigOne.actor.Player;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        Player player = PlayerCreator.createPlayer();
+        List<Monster> monsters = MonsterCreationUtil.createMonster();
 
 
+        monsters.get(0).receiveDamage(player.attack());
+        monsters.stream().filter(m -> m.isAlive())
+                .forEach(singleMonster -> player.receiveDamage(singleMonster.attack()));
+        System.out.println(monsters);
     }
-
-
 }
+
