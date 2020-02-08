bb0a4298e9c1a6699565b98ccb9b36a26e2056a8
Mateusz Grzeskowiak <mateusz.r.grzeskowiak@gmail.com>
2020-02-08 14:47:10 +0100

    Loop attack added

--- src/main/java/pl/sda/bigOne/App.java
+++ src/main/java/pl/sda/bigOne/App.java
@@ -6,17 +6,26 @@ import pl.sda.bigOne.actor.Player;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) {
         Player player = PlayerCreator.createPlayer();
         List<Monster> monsters = MonsterCreationUtil.createMonster();
 
+        while (player.isAlive() && monsters.stream().anyMatch(m -> m.isAlive())) {
+            monsters.stream()
+                    .filter(m -> m.isAlive())
+                    .findFirst()
+                    .get()
+                    .receiveDamage(player.attack());
 
-        monsters.get(0).receiveDamage(player.attack());
-        monsters.stream().filter(m -> m.isAlive())
-                .forEach(singleMonster -> player.receiveDamage(singleMonster.attack()));
-        System.out.println(monsters);
+            monsters.stream().filter(m -> m.isAlive())
+                    .forEach(singleMonster -> player.receiveDamage(singleMonster.attack()));
+            System.out.println(monsters);
+
+        }
+
     }
 }
 
